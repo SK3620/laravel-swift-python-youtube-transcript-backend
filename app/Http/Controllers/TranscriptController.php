67b51391b35e406f04b3bec6605a1cb7f002ae37
SK3620 <k.n.t11193620@gmail.com>
2024-08-06 12:25:35 +0900

    Add:  コントローラーにPythonスクリプトファイルの実行処理を再記述

--- app/Http/Controllers/TranscriptController.php
+++ app/Http/Controllers/TranscriptController.php
@@ -3,8 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
 
 class TranscriptController extends Controller
 {
-    //
+    public function getTranscript($video_id)
+    {
+        $scriptPath = base_path('sample-laravel-flutter-youtube-transcript-env/scripts/get_transcript.py'); // Pythonスクリプトのパス
+
+        $process = new Process(['python3', $scriptPath, $video_id]);
+        $process->run();
+
+
+
+        // エラー処理
+        if (!$process->isSuccessful()) {
+            throw new ProcessFailedException($process);
+        }
+
+
+        $transcript = $process->getOutput();
+        return response()->json(json_decode($transcript, true));
+    }
 }
